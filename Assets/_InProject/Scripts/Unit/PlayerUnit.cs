4efd072f9a6f64441372d187524605b7933881d3
skfksky1004 <tktls13@gmail.com>
2026-09-07 20:53:48 +0900

    Reduce BunnyGirl head forward offset to 0.05

--- Assets/_InProject/Scripts/Unit/PlayerUnit.cs
+++ Assets/_InProject/Scripts/Unit/PlayerUnit.cs
@@ -31,7 +31,7 @@ public class PlayerUnit : UnitBase
     public UnitHairParts hairSkin_front;
     public UnitHairParts hairSkin_base;
 
-    private const float HeadForwardOffset = 0.055f;
+    private const float HeadForwardOffset = 0.05f;
     
 
     private readonly List<GameObject> _createdObjects = new();
